e11cb7e5878992b953557c24be01ed8a7d99a1c1
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-01 23:15:04 -0500

    Add: Title variable

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,7 @@
 
 <html>
 <head>
-	<title><?php echo "Personal Media Library"?></title>
+	<title><?php echo $pageTitle?></title>
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 <body>
