211246159c2ef76641b81df0699b65047247988c
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-02 14:48:43 -0500

    Add: $_GET conditional

--- catalog.php
+++ catalog.php
@@ -1,9 +1,19 @@
 <?php
+//Default category
 $pageTitle = "Full Catalog";
+//Check category and display associated page
+if($_GET["cat"] == "books"){
+    $pageTitle = "Books";
+}else if($_GET["cat"] == "movies"){
+    $pageTitle = "Movies";
+}else if($_Get["cat"] == "music"){
+    $pageTitle = "Music";
+}
 include("inc/header.php");
 ?>
 <div class ="section page">
-    <h1>Full Catalog</h1>
+    <!--Add: page title -->
+    <h1><?php echo $pageTitle; ?></h1>
 </div>
 <?php
 include("inc/footer.php");
