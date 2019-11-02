ce8fcd4db9f0a6adc916ad1b22b7a443a3e73096
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-02 15:04:35 -0500

    Update: Isset for valid query string

--- catalog.php
+++ catalog.php
@@ -1,14 +1,17 @@
 <?php
 //Default category
-$pageTitle = "Full Catalog";
+    $pageTitle = "Full Catalog";
+//Check if cat is valid query string from nav
+    if(isset($_Get["cat"])){
 //Check category and display associated page
-if($_GET["cat"] == "books"){
-    $pageTitle = "Books";
-}else if($_GET["cat"] == "movies"){
-    $pageTitle = "Movies";
-}else if($_Get["cat"] == "music"){
-    $pageTitle = "Music";
-}
+        if($_GET["cat"] == "books"){
+            $pageTitle = "Books";
+        }else if($_GET["cat"] == "movies"){
+            $pageTitle = "Movies";
+        }else if($_Get["cat"] == "music"){
+            $pageTitle = "Music";
+        }
+    }
 include("inc/header.php");
 ?>
 <div class ="section page">
@@ -16,5 +19,5 @@ include("inc/header.php");
     <h1><?php echo $pageTitle; ?></h1>
 </div>
 <?php
-include("inc/footer.php");
+    include("inc/footer.php");
 ?>
