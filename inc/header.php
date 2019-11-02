5b4077039c9a47add99b059bd9c3bf2a6cfa2b57
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-02 15:00:31 -0500

    Add: Query string to navigation

--- inc/header.php
+++ inc/header.php
@@ -13,10 +13,10 @@
 
 			<h1 class="branding-title"><a href="/">Personal Media Library</a></h1>
 
-			<ul class="nav">
-                <li class="books"><a href="#">Books</a></li>
-                <li class="movies"><a href="#">Movies</a></li>
-                <li class="music"><a href="#">Music</a></li>
+			<ul class="nav"> 			<!--Add query string --> 
+                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
+                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
+                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
                 <li class="suggest"><a href="suggest.php">Suggest</a></li>
             </ul>
 
